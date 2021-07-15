5a522a1529aa71a41004c2ae39882c43aa112b20
thienchi1004 <you@example.com>
2021-07-15 19:26:05 +0700

    Add Product API

---
+++ Ecom.CustomerSite/Services/ProductClient/IProductApiClient.cs
@@ -0,0 +1,16 @@
+﻿using Ecom.Backend.Models;
+using Ecom.Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecom.CustomerSite.Services.ProductClient
+{
+	interface IProductApiClient
+	{
+		Task<List<ProductVm>> GetListProduct();
+		Task<ProductVm> GetById(int id);
+		Task<List<ProductVm>> GetByCategory(int id);
+	}
+}
